b10eb9f7fef20d115506a2d238ecd8bfd7456fb4
637chandan <mailtochandansrivastava@gmail.com>
2021-01-08 00:58:35 +0530

    Moview Browser Implementation Frontend  - 1 - Added Dependency - Card View, Recycler View  etc - Added Activity - Added Resources - Added Adapter -

---
+++ movieboard/src/main/java/com/chandan/moviebrowser/data/Movie.kt
@@ -0,0 +1,12 @@
+package com.chandan.moviebrowser.data
+
+data class Movie(
+    var title: String,
+
+    var desc: String,
+
+    var image: String,
+
+    var year: String
+) {
+}
---
+++ movieboard/src/main/java/com/chandan/moviebrowser/ui/activity/HomePage.kt
@@ -0,0 +1,12 @@
+package com.chandan.moviebrowser.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chandan.moviebrowser.R
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
